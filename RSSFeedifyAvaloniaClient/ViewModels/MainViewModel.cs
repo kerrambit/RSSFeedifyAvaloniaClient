6884db096bef97abdff4ec53d168c8f23eb5b772
Marek Eibel <536376@mail.muni.cz>
2024-09-18 19:19:32 +0200

    feat: MainView can show secret page without login page is the user's credentials are stored locally (#4)
    https://grove-sandwich-1dd.notion.site/Views-ViewModels-Create-Login-Register-pages-d52bdfe4be5349418c625bdcc2144e52?pvs=4

--- RSSFeedifyAvaloniaClient/ViewModels/MainViewModel.cs
+++ RSSFeedifyAvaloniaClient/ViewModels/MainViewModel.cs
@@ -5,8 +5,17 @@ namespace RSSFeedifyAvaloniaClient.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
+    private bool _loggedIn = false;
+
     public MainViewModel()
     {
+        // Application may remember the last user (name & password) and will login automatically.
+        if (_loggedIn)
+        {
+            CurrentPage = new UserMainDashboardViewModel(this);
+            return;
+        }
+
         CurrentPage = new LoginViewModel(this);
     }
 }
