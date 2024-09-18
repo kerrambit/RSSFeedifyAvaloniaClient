30aae145edf00d884fcbf7fc78123eec91015042
Marek Eibel <536376@mail.muni.cz>
2024-09-18 19:18:35 +0200

    feat: prepare secret view after login page (#4)
    https://grove-sandwich-1dd.notion.site/Views-ViewModels-Create-Login-Register-pages-d52bdfe4be5349418c625bdcc2144e52?pvs=4

---
+++ RSSFeedifyAvaloniaClient/ViewModels/UserMainDashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RSSFeedifyAvaloniaClient.ViewModels;
+
+public partial class UserMainDashboardViewModel : ViewModelBase
+{
+    private readonly MainViewModel _mainViewModel;
+
+    [ObservableProperty]
+    private string _jWT = string.Empty;
+
+    public UserMainDashboardViewModel(MainViewModel mainViewModel)
+    {
+        _mainViewModel = mainViewModel;
+    }
+
+
+    
+}
---
+++ RSSFeedifyAvaloniaClient/Views/UserMainDashboardView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RSSFeedifyAvaloniaClient.Views;
+
+public partial class UserMainDashboardView : UserControl
+{
+    public UserMainDashboardView()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
