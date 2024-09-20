77e86c5aa951091dce923672c948184be3e058b0
Marek Eibel <536376@mail.muni.cz>
2024-09-20 10:47:00 +0200

    feat: add EmailValidator (#5)
    https://grove-sandwich-1dd.notion.site/Views-ViewModels-Inspect-Implement-view-model-properties-validation-077ab8d4f2de4bb1902196d3b8301492?pvs=4

---
+++ RSSFeedifyAvaloniaClient/Services/Validation/EmailValidator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace RSSFeedifyAvaloniaClient.Services.Validation
+{
+    public class EmailValidator
+    {
+        public static bool Validate(in string value, out string result)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                result = string.Empty;
+                return false;
+            }
+
+            try
+            {
+                result = Regex.Replace(value, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                string DomainMapper(Match match)
+                {
+                    var idn = new IdnMapping();
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                result = string.Empty;
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                result = string.Empty;
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(value,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                result = string.Empty;
+                return false;
+            }
+        }
+    }
+}
