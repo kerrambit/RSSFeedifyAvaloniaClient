3f1bc0193139df96d8e315c1fb075b7052271222
Marek Eibel <536376@mail.muni.cz>
2024-09-21 12:18:51 +0200

    feat: implement new view model and view for tab view (class documentitem is temporary) (#9)
    https://grove-sandwich-1dd.notion.site/Views-ViewModels-Create-UserMainDashboard-1079578a2f5080729ddffb8e320be6df?pvs=4

---
+++ RSSFeedifyAvaloniaClient/ViewModels/UserMainDashboardTabsViewModel.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using FluentAvalonia.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSFeedifyAvaloniaClient.ViewModels
+{
+    public partial class UserMainDashboardTabsViewModel : ViewModelBase
+    {
+        public ObservableCollection<DocumentItem> Documents { get; } = new ObservableCollection<DocumentItem>();
+
+        public UserMainDashboardTabsViewModel()
+        {
+            Documents.Add(new DocumentItem("0"));
+            Documents.Add(new DocumentItem("1"));
+        }
+
+        [RelayCommand]
+        private void AddDocument(object obj)
+        {
+            Documents.Add(new DocumentItem($"{Documents.Count + 1}"));
+        }
+
+        [RelayCommand]
+        private void CloseTab(object obj)
+        {
+            Documents.Remove((DocumentItem)obj);
+        }
+    }
+
+    public class DocumentItem
+    {
+        public string Header { get; set; }
+
+        public DocumentItem(string header)
+        {
+            Header = header;
+        }
+
+        public bool Equals(DocumentItem? other)
+        {
+            // Check if the other object is null
+            if (other is null) return false;
+
+            // Check if the headers are equal
+            return string.Equals(Header, other.Header, StringComparison.Ordinal);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            // If the object is not a DocumentItem, return false
+            if (obj is not DocumentItem other) return false;
+            return Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Header?.GetHashCode() ?? 0; // Use null-coalescing to handle null header
+        }
+    }
+}
+
