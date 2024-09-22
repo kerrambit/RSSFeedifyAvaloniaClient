abc7b9042ec94ca32f19995852f35dbd3a3fc73c
Marek Eibel <536376@mail.muni.cz>
2024-09-22 10:17:58 +0200

    feat: implement loader for call to API to get user's categories - as this endpoint does not exist, it is being mocked (#9)
    https://grove-sandwich-1dd.notion.site/Views-ViewModels-Create-UserMainDashboard-1079578a2f5080729ddffb8e320be6df?pvs=4

---
+++ RSSFeedifyAvaloniaClient/Services/API/IUserSpecifiedPopulatedRSSFeedCategoriesLoader.cs
@@ -0,0 +1,15 @@
+﻿using RSSFeedifyAvaloniaClient.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSFeedifyAvaloniaClient.Services.API
+{
+    public interface IUserSpecifiedPopulatedRSSFeedCategoriesLoader
+    {
+        // TODO: the result type will be replaced by RSSFeedifyCommon type
+        Task<IList<NavigationItem>> LoadAsync();
+    }
+}
