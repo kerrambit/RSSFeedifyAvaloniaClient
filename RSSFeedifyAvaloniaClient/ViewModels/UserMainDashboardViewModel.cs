357ff0793af19700dbb21f0ff764c57714bf71ec
Marek Eibel <536376@mail.muni.cz>
2024-09-22 10:19:24 +0200

    feat: implement auto generation of nav view with items from API (#9)
    https://grove-sandwich-1dd.notion.site/Views-ViewModels-Create-UserMainDashboard-1079578a2f5080729ddffb8e320be6df?pvs=4

--- RSSFeedifyAvaloniaClient/ViewModels/UserMainDashboardViewModel.cs
+++ RSSFeedifyAvaloniaClient/ViewModels/UserMainDashboardViewModel.cs
@@ -1,15 +1,48 @@
-﻿namespace RSSFeedifyAvaloniaClient.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RSSFeedifyAvaloniaClient.DataModels;
+using RSSFeedifyAvaloniaClient.Services.API;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace RSSFeedifyAvaloniaClient.ViewModels;
 
 public partial class UserMainDashboardViewModel : ViewModelBase
 {
     private readonly MainViewModel _mainViewModel;
 
+    [ObservableProperty]
+    private NavigationItem _navigationItem;
+    public string? JWT => _mainViewModel.UserJWT;
+
+    public ObservableCollection<NavigationItem> NavigationViewItems { get; } = new ObservableCollection<NavigationItem>();
+    public UserMainDashboardViewModel() { }
     public UserMainDashboardViewModel(MainViewModel mainViewModel)
     {
         _mainViewModel = mainViewModel;
+
+        NavigationViewItems.Add(new("All Feeds", "All Feeds", "PageFilled"));
+        NavigationViewItems.Add(new("Watched Feeds", "Watched Feeds", "PageFilled"));
+        NavigationViewItems.Add(new("Recommended", "Recommended", "PageFilled"));
+
+        NavigationItem = NavigationViewItems[0];
+        _ = LoadUserSpecificNavigationViewItemsAsync(new MockedUserSpecifiedPopulatedRSSFeedCategoriesLoader());
     }
 
-    public UserMainDashboardViewModel() { }
+    // This is mocked method for now.
+    // TODO: RSSFeedify API must offer endpoint to retrieve user's categories.
+    public async Task LoadUserSpecificNavigationViewItemsAsync(IUserSpecifiedPopulatedRSSFeedCategoriesLoader loader)
+    {
+        var data = await loader.LoadAsync();
 
-    public string? JWT => _mainViewModel.UserJWT;
+        foreach (var item in data)
+        {
+            if (item.SubItems.Count > 0)
+            {
+                item.IconSource = "LibraryFilled";
+            }
+
+            NavigationViewItems.Add(item);
+        }
+    }
+
 }
